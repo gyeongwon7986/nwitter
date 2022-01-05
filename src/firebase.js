9f4509d4a6548aab7852d8f4a739d4abd16a517d
gyeongwon7986 <gyeongwon7986@gmail.com>
2022-01-05 16:40:41 +0900

    1.0 Initialization

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/compat/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCLVq4j81ktYsJeu38cdAx_NMGiSk-9EDQ",
+    authDomain: "nwitter-8048d.firebaseapp.com",
+    projectId: "nwitter-8048d",
+    storageBucket: "nwitter-8048d.appspot.com",
+    messagingSenderId: "2247441962",
+    appId: "1:2247441962:web:1b60f622417d897aa64d53"
+  };
+
+export default firebase.initializeApp(firebaseConfig);
